e133c0d871843ea330265eb1f7b5e5bacbde9b83
zkan <kan@prontomarketing.com>
2020-03-28 23:26:26 +0700

    :sparkles: Create new component and use v-for for looping :bear:

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <Main />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Main
   }
 }
 </script>
---
+++ src/components/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main">
+    {{text}}
+    <ul>
+      <li v-for="todo in todos" :key="todo.time">
+        {{todo.text}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      text: 'Hello World',
+      todos: [
+        {
+          text: '<strong>Todo1</strong>',
+          time: 12345,
+          completed: true
+        },
+        {
+          text: 'Todo2',
+          time: 12541,
+          completed: false
+        }
+      ]
+    }
+  }
+}
+</script>
